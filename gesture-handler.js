0161bbd22534b88084096098d0f271ffe9d6d74d
mavverie <128604315+ddandelioonn@users.noreply.github.com>
2025-01-23 14:53:27 +0500

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -23,23 +23,25 @@ AFRAME.registerComponent("gesture-handler", {
   },
 
   handleRotation: function (event) {
-    this.el.object3D.rotation.y +=
-      event.detail.positionChange.x * this.data.rotationFactor;
-    this.el.object3D.rotation.x +=
-      event.detail.positionChange.y * this.data.rotationFactor;
+    // Проверяем, что есть данные для изменения
+    if (event.detail && event.detail.positionChange) {
+      const positionChange = event.detail.positionChange;
+      this.el.object3D.rotation.y += positionChange.x * this.data.rotationFactor;
+      this.el.object3D.rotation.x += positionChange.y * this.data.rotationFactor;
+    }
   },
 
   handleScale: function (event) {
-    this.scaleFactor *=
-      1 + event.detail.spreadChange / event.detail.startSpread;
+    // Проверяем, что есть данные для изменения
+    if (event.detail && event.detail.spreadChange !== undefined) {
+      this.scaleFactor *= 1 + event.detail.spreadChange / event.detail.startSpread;
 
-    this.scaleFactor = Math.min(
-      Math.max(this.scaleFactor, this.data.minScale),
-      this.data.maxScale
-    );
+      // Ограничиваем масштаб в пределах минимального и максимального значений
+      this.scaleFactor = Math.min(Math.max(this.scaleFactor, this.data.minScale), this.data.maxScale);
 
-    this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-    this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-    this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
+      this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
+      this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
+      this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
+    }
   },
 });
