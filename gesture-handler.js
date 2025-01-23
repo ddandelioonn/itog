a0a130042ca438fe156b4218a6f20a5e5f55f057
mavverie <128604315+ddandelioonn@users.noreply.github.com>
2025-01-23 15:13:22 +0500

    Update gesture-handler.js

--- gesture-handler.js
+++ gesture-handler.js
@@ -1,67 +1,27 @@
-/* global AFRAME, THREE */
-
-AFRAME.registerComponent("gesture-handler", {
-  schema: {
-    enabled: { default: true },
-    rotationFactor: { default: 5 },
-    minScale: { default: 0.3 },
-    maxScale: { default: 8 },
-  },
-
+AFRAME.registerComponent('gesture-handler', {
   init: function () {
-    this.handleScale = this.handleScale.bind(this);
-    this.handleRotation = this.handleRotation.bind(this);
-
-    this.isVisible = false;
-    this.initialScale = this.el.object3D.scale.clone();
-    this.scaleFactor = 1;
-
-    this.el.sceneEl.addEventListener("markerFound", (e) => {
-      this.isVisible = true;
-    });
-
-    this.el.sceneEl.addEventListener("markerLost", (e) => {
-      this.isVisible = false;
-    });
+    this.el.addEventListener('mousedown', this.startGesture.bind(this));
+    this.el.addEventListener('touchstart', this.startGesture.bind(this));
+    this.el.addEventListener('mousemove', this.handleGesture.bind(this));
+    this.el.addEventListener('touchmove', this.handleGesture.bind(this));
+    this.el.addEventListener('mouseup', this.endGesture.bind(this));
+    this.el.addEventListener('touchend', this.endGesture.bind(this));
   },
 
-  update: function () {
-    if (this.data.enabled) {
-      this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
-      this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
-    } else {
-      this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
-      this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
-    }
+  startGesture: function (event) {
+    // Инициализация жеста
+    this.isGesturing = true;
+    this.initialDistance = event.touches ? event.touches[0].clientX : event.clientX;
   },
 
-  remove: function () {
-    this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
-    this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
+  handleGesture: function (event) {
+    if (!this.isGesturing) return;
+    const distance = event.touches ? event.touches[0].clientX : event.clientX;
+    const scale = (distance - this.initialDistance) * 0.01;
+    this.el.emit('pinch', { scale });
   },
 
-  handleRotation: function (event) {
-    if (this.isVisible) {
-      this.el.object3D.rotation.y +=
-        event.detail.positionChange.x * this.data.rotationFactor;
-      this.el.object3D.rotation.x +=
-        event.detail.positionChange.y * this.data.rotationFactor;
-    }
-  },
-
-  handleScale: function (event) {
-    if (this.isVisible) {
-      this.scaleFactor *=
-        1 + event.detail.spreadChange / event.detail.startSpread;
-
-      this.scaleFactor = Math.min(
-        Math.max(this.scaleFactor, this.data.minScale),
-        this.data.maxScale
-      );
-
-      this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-      this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-      this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
-    }
-  },
+  endGesture: function () {
+    this.isGesturing = false;
+  }
 });
